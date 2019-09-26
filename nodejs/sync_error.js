f528a653112ce3b3864173c849353b948b45ca2e
PaulHan <zhangyu.han@outlook.com>
2019-09-27 07:58:28 +0800

    fix: async error comment

--- nodejs/sync_error.js
+++ nodejs/sync_error.js
@@ -4,4 +4,4 @@ function a() {
   }, 100);
 }
 
-a(); // 浏览器中调用栈包含a（对于async代码会画一条线作区分），nodejs中没有（栈底Timer.processTimers），csharp中也一样（栈底System.Threading.Tasks.Task.Execute()）
+a(); // 浏览器中没有（只有在断点调试时，调用栈信息才包含函数a，对于async代码会画一条线作区分），nodejs中没有（栈底Timer.processTimers），csharp中也一样（栈底System.Threading.Tasks.Task.Execute()）
